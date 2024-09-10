1019e803d143d5c7f50a657e892a527806906337
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2024-09-10 07:42:06 +0200

    update

---
+++ 1833357106319130718.R
@@ -0,0 +1,33 @@
+library("tidyverse")
+
+data <- "001762445+001762847+001759971" |>
+  paste0("https://www.bdm.insee.fr/series/sdmx/data/SERIES_BDM/", a = _) |>
+  rsdmx::readSDMX() |>
+  as_tibble()  |>
+  mutate(date = as.Date(paste0(TIME_PERIOD, "-01")),
+         OBS_VALUE = as.numeric(OBS_VALUE)) |>
+  filter(date >= as.Date("2021-07-01"),
+         date <= as.Date("2024-07-01")) |>
+  # Extraire après le dernier tiret -----
+  mutate(Coicop2016 = str_extract(TITLE_FR, "[^-]+$")) |>
+  arrange(date) |>
+  group_by(Coicop2016) |>
+  mutate(OBS_VALUE = 100*OBS_VALUE/OBS_VALUE[1])
+
+data |>
+  ggplot() + ylab("") + xlab("") + theme_minimal() +
+  geom_line(aes(x = date, y = OBS_VALUE, color = Coicop2016)) +
+  theme_minimal() + xlab("") + ylab("") +
+  scale_x_date(breaks = "2 months",
+               labels = date_format("%b %Y")) +
+  theme(legend.position = c(0.28, 0.87),
+        legend.title = element_blank(),
+        axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) +
+  scale_y_log10(breaks = seq(0, 200, 2),
+                labels = dollar_format(accuracy = 1, prefix = "")) +
+  geom_label(data = . %>% filter(date == max(date)),
+             aes(x = date, y = OBS_VALUE, label = round(OBS_VALUE, 1), color = Coicop2016), 
+             fontface ="plain", size = 3)
+
+ggsave("1833357106319130718.png", height = 1.25*3.375, width = 1.25*6, bg = "white")
+ggsave("1833357106319130718.pdf", height = 1.25*3.375, width = 1.25*6)
